e38f4af837dfb9281ee6ec1452a7d671ad0f25ce
Miloszek1990 <miloszmartynow@gmail.com>
2020-07-14 18:25:45 +0200

    Working image sending over sockets

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,14 +6,16 @@ class Server
     private:
 		int port_;
         int bufferSize_;
+        int imgWdth_;
+        int imgHght_;
 
         void error( char* );
+        void getFrame(int& sockfd);
 
     public:
-        Server(int, int);
+        Server(int, int, int, int);
         ~Server();
 
-        void getData( int, char* );
         void initializeConnection(int&, sockaddr_in&);
         void streamLoop(int&, int&, sockaddr_in&);
 };
--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <netinet/in.h>
 
-#include "./include/Server.hpp"
+#include <opencv2/opencv.hpp>
 
+#include "./include/Server.hpp"
 
 int main(int argc, char *argv[]) {
     
     int port = 8000;
     int bufferSize = 32;
+    int imgWdth = 1024;
+    int imgHght = 768;
+
     int sockfd, newsockfd;
     sockaddr_in cli_addr;
 
-    Server server(port, bufferSize);
+    Server server(port, bufferSize, imgWdth, imgHght);
     server.initializeConnection(sockfd, cli_addr);
     server.streamLoop(sockfd, newsockfd, cli_addr);
 
